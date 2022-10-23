d71d53274039ee32481d840067b17d51edbfd971
darshsingh116 <darshsingh116@gmail.com>
2022-10-23 23:50:34 +0530

    auth_plugin added

--- bot_database.py
+++ bot_database.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(host="localhost", user="root", passwd="BotBuck123!")
+mydb = mysql.connector.connect(host="localhost", user="root", passwd="BotBuck123!" , auth_plugin='mysql_native_password')
 mycursor = mydb.cursor()
 mycursor.execute("use botbuck")
 
